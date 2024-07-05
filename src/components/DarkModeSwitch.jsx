ed031a964bc1611d1d494e4b67a4dc8a3034037a
aryan <aryansadeghyan54@gmail.com>
2024-07-05 20:48:12 +0330

    added dark mode

--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -1,5 +1,35 @@
-import React from "react";
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { FaMoon } from "react-icons/fa";
+import { MdOutlineWbSunny } from "react-icons/md";
 
 export default function DarkModeSwitch() {
-  return <div>DarkModeSwitch</div>;
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === "stytem" ? systemTheme : theme;
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  return (
+    <div className='cursor-pointer'>
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdOutlineWbSunny
+            className='text-xl hover:text-amber-500'
+            onClick={() => {
+              setTheme("light");
+            }}
+          />
+        ) : (
+          <FaMoon
+            className='text-xl hover:text-amber-500'
+            onClick={() => {
+              setTheme("dark");
+            }}
+          />
+        ))}
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,24 @@ import { FaHome } from "react-icons/fa";
 import { FaCircleInfo } from "react-icons/fa6";
 
 import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className='flex justify-between p-4 max-w-5xl mx-auto mt-[1rem] font-light '>
+    <div className='flex justify-between p-4 max-w-5xl mx-auto pt-7 font-light items-center '>
       <div className='flex gap-10 text-sm'>
         <MenuItem title='HOME' address='/' Icon={FaHome} />
         <MenuItem title='ABOUT' address='/about' Icon={FaCircleInfo} />
       </div>
-      <div>
-        <div className='flex items-center gap-1'>
-          <Link href='/'>
-            <span className='px-2  bg-amber-500 font-bold  rounded text-gray-700 text-xl'>
-              IMDb
-            </span>
-            <span className='text-xl hidden sm:inline'>clone</span>
-          </Link>
-        </div>
+
+      <div className='flex items-center gap-3'>
+        <DarkModeSwitch />
+        <Link href='/' className='flex gap-1'>
+          <span className='px-2  bg-amber-500 font-bold  rounded text-gray-700 text-xl'>
+            IMDb
+          </span>
+          <span className='text-xl hidden sm:inline'>clone</span>
+        </Link>
       </div>
     </div>
   );
