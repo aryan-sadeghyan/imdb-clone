89f2dc42478ea7d0e7818188a7d4424b28af5419
aryan <aryansadeghyan54@gmail.com>
2024-07-05 22:25:24 +0330

    about page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
-    <div className='flex justify-between p-4 max-w-5xl mx-auto pt-7 font-light items-center '>
-      <div className='flex gap-10 text-sm'>
+    <div className='flex justify-between p-4 max-w-6xl mx-auto pt-7 font-light items-center '>
+      <div className='flex gap-10 text-sm '>
         <MenuItem title='HOME' address='/' Icon={FaHome} />
         <MenuItem title='ABOUT' address='/about' Icon={FaCircleInfo} />
       </div>
@@ -17,7 +17,7 @@ export default function Header() {
       <div className='flex items-center gap-3'>
         <DarkModeSwitch />
         <Link href='/' className='flex gap-1'>
-          <span className='px-2  bg-amber-500 font-bold  rounded text-gray-700 text-xl'>
+          <span className=' px-2  bg-amber-500 font-bold  rounded text-gray-700 text-xl'>
             IMDb
           </span>
           <span className='text-xl hidden sm:inline'>clone</span>
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -4,7 +4,7 @@ export default function MenuItem({ title, address, Icon }) {
   return (
     <Link href={address}>
       <Icon className=' sm:hidden text-2xl' />
-      <p className='hidden sm:inline'>{title}</p>
+      <p className='hidden sm:inline hover:text-amber-500'>{title}</p>
     </Link>
   );
 }
