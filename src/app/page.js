777bb7e5e98591dac0ebe73c2208526c55947e8d
aryan <aryansadeghyan54@gmail.com>
2024-07-06 15:44:10 +0330

    fetching data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,27 @@
 import Image from "next/image";
 
-export default function Home() {
-  return <main>Home</main>;
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "Trending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "top_rated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const result = data.results;
+
+  return (
+    <main>
+      <div>
+        {result.map((movie) => {
+          return <div key={movie.id}>{movie.title}</div>;
+        })}
+      </div>
+    </main>
+  );
 }
