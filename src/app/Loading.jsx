8e7a27beb4b8c485b6115f3600965d2d2fc5f636
aryan <aryansadeghyan54@gmail.com>
2024-07-07 18:09:42 +0330

    tweaks for deployment

--- src/app/Loading.jsx
+++ src/app/Loading.jsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Loading() {
   return (
     <div className='flex justify-center mt-16'>
-      <img className='h-10' src='spinner.svg' alt='loading...' />
+      <Image
+        className='h-10'
+        src='spinner.svg'
+        alt='loading'
+        width={50}
+        height={50}
+      ></Image>
     </div>
   );
 }
